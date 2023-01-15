8b232c2b605879e9c00b3eeb982349c639c98bf9
sleepntsheep <contact@papangkorn.com>
2023-01-15 10:03:12 +0700

    readme

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-clang -o sapc src/main.c \
+clang -o sap src/main.c \
     src/sstring.c src/sobject.c src/lexer.c src/parser.c src/ast.c src/token.c src/env.c src/eval.c src/error.c \
     -g -lm\
     -std=c11 -Wall -Wextra -Wimplicit-fallthrough -pedantic
